32f3d16b01cbe234c0b6ed4c08003333940eae40
Fa-biano <fbiano.souza@gmail.com>
2022-10-07 15:57:01 -0300

    iniciando o projeto trivia

---
+++ src/redux/reducers/player.js
@@ -0,0 +1,16 @@
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  name: '',
+  assertions: 0,
+  score: 0,
+  gravatarEmail: '',
+};
+
+const player = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default player;
