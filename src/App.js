10f2b58acf5973e1aa52533d73b428a71193787e
Fa-biano <fbiano.souza@gmail.com>
2022-10-07 18:47:56 -0300

    realizado o requisito 3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
+      <Route exact path="/config" component={ Settings } />
     </Switch>
   );
 }
